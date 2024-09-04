9664b6a3cfa589ca16d0546f8ae6b2c5b9b52c74
lthung1 <167729697+lthung1@users.noreply.github.com>
2024-09-04 11:15:13 +0700

    GRPC service

---
+++ src/main/java/com/vn/grpc/demogrpc/DemoGrpcApplication.java
@@ -0,0 +1,12 @@
+package com.vn.grpc.demogrpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGrpcApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoGrpcApplication.class, args);
+    }
+
+}
